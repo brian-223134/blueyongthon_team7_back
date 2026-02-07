24f3bc01a424c25ffc9db11509c65542a05321aa
정우 <jungwoo@jeong-uui-MacBookAir.local>
2026-02-07 21:33:16 +0900

    chores

--- project/recipes/urls.py
+++ project/recipes/urls.py
@@ -5,7 +5,7 @@ from django.urls import include
 
 urlpatterns = [
     path('', views.RecipeView.as_view(), name='recipe-list'),
-    path('/all/', views.RecipeSharedView.as_view(), name='recipe-shared-list'),
+    path('all/', views.RecipeSharedView.as_view(), name='recipe-shared-list'),
     path('<int:pk>/', views.RecipeDetailView.as_view(), name='recipe-detail'),
     path('<int:recipe_pk>/steps/', views.RecipeStepView.as_view(), name='recipe-steps'),
     path('<int:recipe_pk>/steps/<int:step_pk>/', views.RecipeStepDetailView.as_view(), name='recipe-step-list'),
